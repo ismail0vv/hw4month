66c1fd73617f298bdb94d443ff6b367d8e551598
ismail0vv <asylismailov05@gmail.com>
2022-11-09 23:54:40 +0600

    homework 8

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
 from .models import Film, Director
+
+
 # Register your models here.
-admin.site.register(Film)
-admin.site.register(Director)
+class FilmsInLine(admin.StackedInline):
+    model = Film
+    extra = 1
+
+
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ['director', 'title', 'producer', 'rating', 'duration']
+    search_fields = 'director title producer'.split()
+    list_filter = 'director producer'.split()
+    list_editable = 'rating duration'.split()
+    list_per_page = 100
+
+
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+    list_filter = ['name']
+    list_per_page = 100
+    inlines = [FilmsInLine]
+
+
+admin.site.register(Film, FilmAdmin)
+admin.site.register(Director, DirectorAdmin)
--- main/models.py
+++ main/models.py
@@ -3,19 +3,25 @@ from django.db import models
 
 # Create your models here.
 class Director(models.Model):
-    name = models.CharField(max_length=100)
+    class Meta:
+        verbose_name = 'Режиссер'
+        verbose_name_plural = 'Режиссеры'
+    name = models.CharField(max_length=100, verbose_name='Имя')
 
     def __str__(self):
         return self.name
 
 
 class Film(models.Model):
+    class Meta:
+        verbose_name = 'Фильм'
+        verbose_name_plural = 'Фильмы'
     director = models.ForeignKey(Director, on_delete=models.SET_NULL,
-                                 related_name='film_list_on_director', null=True)
-    title = models.CharField(max_length=255)
-    producer = models.CharField(max_length=255)
-    rating = models.FloatField(blank=True, null=True)
-    duration = models.FloatField(default=0)
+                                 related_name='film_list_on_director', null=True, verbose_name='Режиссер')
+    title = models.CharField(max_length=255, verbose_name='Название')
+    producer = models.CharField(max_length=255, verbose_name='Продюсер')
+    rating = models.FloatField(blank=True, null=True, verbose_name='Рейтинг')
+    duration = models.FloatField(default=0, verbose_name='Длительность')
 
     def __str__(self):
         return self.title
